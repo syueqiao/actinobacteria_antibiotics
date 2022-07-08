349f4a50a5692fbdde680e97b737cfdc12b950f0
syueqiao <jess0773@student.ubc.ca>
2022-07-08 13:15:42 -0700

    commented code and removed redundant sections used for testing

--- turi_dendro_test.R
+++ turi_dendro_test.R
@@ -11,23 +11,28 @@ library("grid")
 library("RColorBrewer")
 library("dendextend")
 library("ape")
+library("fuzzyjoin")
 
 #read the file test1.smiles <- you'll need to set up anything input into the right format
 turi_all_smiles <- read_tsv("turi_all_smiles.txt")
+
+#remove duplicates because pubchem search returns duplicates
 turi_all_smiles <- turi_all_smiles[!duplicated(turi_all_smiles$Chemical),]
 
+#subset out the chemicals pubchem was not able to find smiles for
 turi_smiles_NA <- turi_all_smiles_test[is.na(turi_all_smiles_test$SMILE),]
-turi_smiles_NA <- left_join(turi_smiles_NA, turi_all_NA, by = "Chemical")
-turi_smiles_NA <- subset(turi_smiles_NA, select = -c(SMILE.x))
 write_tsv(turi_smiles_NA, "turi_smiles_NA_new.tsv")
 
-
+#manually edit in the smiles that pubchem was not able to get
 turi_all_NA <- read_tsv("turi_NAs.txt")
 turi_all_NA <- turi_all_NA[!duplicated(turi_all_NA$Chemical),]
 turi_all_smiles <- na.omit(turi_all_smiles)
 
+#combine two dataframes together 
 turi_smiles <- rbind(turi_all_smiles, turi_all_NA)
+#reverse dataframes since chemmineR assumes the first column is smiles, and the second column is the identifier
 turi_smiles <- rev(turi_smiles)
+#write out to import
 write_tsv(turi_smiles, "turi_smiles_import.txt")
 #need to manually add some files that were not able to be automatically retrieved######
 
@@ -57,66 +62,16 @@ load("distmat.rda")
 #convert to matrix format
 sweet_matrix <- as.matrix(distmat)
 
-hc <- hclust(as.dist(distmat), method="ward") 
+#ward.D2 uses non-squared distances in euclidean space. the distance matrix I have is not euclidean
+#however, since we are not analyzing clusters rigourously and more for visualization purposes, should be okay
+#previously erroneously used "ward" which should have squared values input
+hc <- hclust(as.dist(distmat), method="ward.D2") 
 hc[["labels"]] <- cid(apset) # Assign correct item labels 
-plot(as.dendrogram(hc), edgePar=list(col=50, lwd=2), horiz=T)
 
-png(filename = "testy5.png", res = 300,
-    width = 10000, height = 10000)
-t <- plot(as.phylo(hc), type = "unrooted", cex = 0.2, lab4ut = "axial", 
-     no.margin = TRUE)
-plot(hc, hang = -1, cex = 0.3)
-dev.off()
-
+#assign the hierarchal clustering output as a phylogenetic tree
+#write out as a tree
 t <- as.phylo(hc)
 write.tree(t, "test_tree2.txt")
-
-p <- plot(as.phylo(hc), type = "unrooted", cex = 0.2, lab4ut = "axial", label.offset = 1,
-     no.margin = TRUE)
-
-png(filename = "testy2.png", res = 300,
-    width = 2000, height = 2000)
-
-plot(as.phylo(hc), type = "unrooted", cex = 0.4,
-     no.margin = TRUE)
-dev.off()
-
-#use the hclust function in R to cluster them based on distance matrix
-sweet_dendro <- as.dendrogram(hclust(d = dist(x = sweet_matrix)))
-#plot w/ ggdendrogram
-dendro_plot <- ggdendrogram(data = sweet_dendro, rotate = TRUE)
-dendro_plot
-#import the categories file. I made a few adjusments to this and you might have to as well
-turi_Me_cat <- read.csv("turi_Me_categories.csv")
-turi_Me_cat <- select(turi_Me_cat, c("index", "well", "category", "bac"))
-
-
-#order the y-axis in the order of the dendrogram tree   
-sweet_order <- order.dendrogram(sweet_dendro)
-turi_Me_cat$index <- factor(x = turi_Me_cat$index,
-                            levels = turi_Me_cat$index[sweet_order], 
-                            ordered = TRUE)
-
-
-
-#repeat lines 56-58 for each bac and solvent
-
-#plot heatmap of categories
-heatmap_plot <- ggplot(data = turi_Me_cat, aes(x = bac, y = index)) +
-  geom_tile(aes(fill = category)) +
-  scale_fill_distiller(palette = "PRGn") +
-  theme(axis.text.y = element_text(size = 6), legend.position = "top")
-#show
-heatmap_plot    
-
-#use grid function to slap the two figures together. need to manually adjust to make things line up
-grid.newpage()
-print(heatmap_plot, 
-      vp = viewport(x = 0.4, y = 0.5, width = 0.8, height = 1.0))
-print(dendro_plot, 
-      vp = viewport(x = 0.90, y = 0.47, width = 0.2, height = 1.03))
-
-#repeat lines 62-75 for each bac and solvent
 
 #do stuff that allows a category to be assignd to each label, basically, 
 
@@ -129,8 +84,6 @@ colnames(platemap_H2O_merge) <- c("well", "solv")
 turi2_H2O_out_merge <- merge(turi2_H2O$cat, platemap_H2O_merge, by = "well")
 
 
-
-
 platemap_MeOH_merge <- platemap_MeOH
 colnames(platemap_MeOH_merge) <- c("well", "solv")
 turi2_MeOH_out_merge <- merge(turi2_MeOH$cat, platemap_MeOH_merge, by = "well")
@@ -138,20 +91,34 @@ turi2_MeOH_out_merge <- merge(turi2_MeOH$cat, platemap_MeOH_merge, by = "well")
 turi2_all_out_merge <- rbind(turi2_MeOH_out_merge, turi2_H2O_out_merge, turi2_DMSO_out_merge)
 
 #perform "fuzzy merge"
-
+#make dataframe with the labels
+labels_df <- data.frame(cid(apset))
 colnames(labels_df) <- c("solv")
 turi2_all_out_fuzzy_jw <- stringdist_join(labels_df, turi2_all_out_merge, method = "jw", by = "solv", max_dist = 99, distance_col = 'dist') %>% group_by(solv.x) %>% slice_min(order_by = dist, n=1)
 
 write.csv(turi2_all_out_fuzzy_jw, "turi_out_fuzzy_jw.csv")
 
 #DO NOT EDIT THE OUTPUT FILE HERE IT WILL BE SCREWED UP AND HARD TO UNDO
+#basically, go through manually and make sure that the fuzzy merge was done correctly
+#sort by distance, the higher the "distance" the more likely that the matching was done incorrectly
+
+#import edited version
 turi_all_out_fuzzy_jw_edited <- read.csv("turi_out_fuzzy_jw_edited.csv")
 
+
+#from this point on you can filter and make trees for each category assigned
 #tree with only inhibitors
 turi2_inhib_out_merge <- filter(turi_all_out_fuzzy_jw_edited, category == "9" | category == "8")
+
+#grab chemical labels that are the inhibitors
 inhibs <- data.frame(turi2_inhib_out_merge$solv.x)
 colnames(inhibs) <- c("Chemical")
+
+#once again, fuzzy merge with the list of smiles, so that you can get the smiles for each chemical
+#i fuzzy merge here because the cid(apset) function seems to remove the spaces
+#this is generally very consistent and won't need to manually change much
 inhibs_smile <- stringdist_join(inhibs, turi_smiles, method = "jw", by = "Chemical", max_dist = 99, distance_col = "dist") %>% group_by(Chemical.x) %>% slice_min(order_by = dist, n=1)
+#clean up format for putting into clustering program
 inhibs_smile <- select(inhibs_smile, Chemical.x, SMILE)
 inhibs_smile <- rev(inhibs_smile)
 write_tsv(inhibs_smile, "turi_inhibs_smiles_import.txt")
